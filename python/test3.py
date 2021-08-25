f5dad567b52cebab8a5b85bd22638d4174aee5f7
Ephram Kukartsev <ephram.kukartsev@gmail.com>
2021-08-24 18:16:24 -0700

    my changes

---
+++ python/test3.py
@@ -0,0 +1,7 @@
+#from test2 import fibonacci
+import test2
+
+if __name__=='__main__':
+    number = input("How many numbers of the Fibinachi Sequence do you want to print?")
+    #fibonacci(number)
+    test2.fibonacci(number)
